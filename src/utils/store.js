3b87fcf0bc7323018dbf51ac64780f849e5f42c9
AV Technosys <developer@avtechnosys.com>
2025-02-01 19:47:57 +0530

    Update store.js changed fetch URL path

--- src/utils/store.js
+++ src/utils/store.js
@@ -3,9 +3,9 @@ import { userReducer } from './User/userSlice';
 import { vehicleReducer } from './vehicle/vehicleSlice';
 
 export const REACT_APP_BASE_URL =
-  // import.meta.env.VITE_BASE_URL || "https://secret-eye-backend.vercel.app";
+   import.meta.env.VITE_BASE_URL || "https://secret-eye-backend.vercel.app";
 
-"http://localhost:5000";
+// "http://localhost:5000";
 // "http://127.0.0.1:5000"
   
 
